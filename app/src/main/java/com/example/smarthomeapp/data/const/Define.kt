4de760540fee64a830a1f0c633a7da7ce00802d5
vietcong00 <57753943+vietcong00@users.noreply.github.com>
2022-02-13 17:30:22 +0700

    final

--- app/src/main/java/com/example/smarthomeapp/data/const/Define.kt
+++ app/src/main/java/com/example/smarthomeapp/data/const/Define.kt
@@ -1,6 +1,6 @@
 package com.example.smarthomeapp.data.const
 
 object Define {
-//    const val BASE_URL = "https://912a-42-114-170-165.ngrok.io"
-const val BASE_URL = "http://13.229.105.101:4000"
+    const val BASE_URL = "https://912a-42-114-170-165.ngrok.io"
+//const val BASE_URL = "http://13.229.105.101:4000"
 }
